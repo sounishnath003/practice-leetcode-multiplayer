cad40129efce7cae5ab4f588a688f6b2af92d4e8
Sounish Nath <flock.sinasini@gmail.com>
2025-03-29 13:57:24 +0530

    okkk

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -6,14 +6,29 @@ import (
 )
 
 var (
-	ErrorSlug = fmt.Errorf("no slug provided or no data found")
+	ErrorSlug   = fmt.Errorf("no slug provided or no data found")
+	ErrNotFound = fmt.Errorf("not found")
 )
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		SendErrorResponse(w, http.StatusNotFound, ErrNotFound)
+		return
+	}
+	if r.Method != http.MethodGet {
+		SendErrorResponse(w, http.StatusNotFound, ErrNotFound)
+		return
+	}
+
+	http.ServeFile(w, r, "templates/index.html")
+}
 
 // HealthHandler handles the API healthz params.
 func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	SendJSONResponse(w, http.StatusOK, "hurray. api is working fine.")
 }
 
+// SearchQuestionHandler
 func SearchQuestionHandler(w http.ResponseWriter, r *http.Request) {
 	question_slug := r.URL.Query().Get("query")
 	// When slug are not provided nicely
