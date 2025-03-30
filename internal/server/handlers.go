482e31fef60f5c0862014fcdb1925889562be2a5
Sounish Nath <flock.sinasini@gmail.com>
2025-03-30 08:18:48 +0530

    Refactor question block layout and enhance description formatting for improved readability

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -63,23 +63,23 @@ func SearchQuestionHandler(w http.ResponseWriter, r *http.Request) {
 		You can return the answer in any order.
 		</p>
 
-		<code>
-		<pre>
-		Example 1:
+<code>
+<pre>
+Example 1:
 
-		Input: nums = [2,7,11,15], target = 9
-		Output: [0,1]
-		Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-		Example 2:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
 
-		Input: nums = [3,2,4], target = 6
-		Output: [1,2]
-		Example 3:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
 
-		Input: nums = [3,3], target = 6
-		Output: [0,1]
-		</pre>
-		</code>
+Input: nums = [3,3], target = 6
+Output: [0,1]
+</pre>
+</code>
 		
 		<p>
 		Constraints:
